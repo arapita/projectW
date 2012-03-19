83357ccf441a03cb9d5f4181d8c4015a74a4b94a
Andrzej Rapita <g4mba89@gmail.com>
2012-03-19 18:09:55 +0100

    a bit changes

--- app/models/pojazdy.rb
+++ app/models/pojazdy.rb
@@ -5,8 +5,6 @@ class Pojazdy < ActiveRecord::Base
     :message => "moze skladac sie tylko z liter" }, :presence => true
   validates :stan_licznika, :numericality => { :greater_than => 1, 
     :message => " musi byc wiekszy niz 0" }, :presence => true
-  validates :lokalizacja, :format => { :with => /\A[a-zA-Z]+\z/,
-    :message => "moze skladac sie tylko z liter" }, :presence => true
   validates :nr_rejestracyjnyn, :length => { :in => 7..8,
     :message => "musi skladac sie z 7-8 znakow"}, :presence => true
   validates :marka, :presence => true
--- app/models/wyjazdy.rb
+++ app/models/wyjazdy.rb
@@ -1,12 +1,14 @@
 class Wyjazdy < ActiveRecord::Base
 
   after_save :zapis_lokacji
+  #after_save :dane
   belongs_to :pojazdy, :foreign_key => "nr_rej", :primary_key => "nr_rejestracyjnyn"
 
   def zapis_lokacji
 
     self.pojazdy.lokalizacja = self.miejsce
     self.pojazdy.save
+
 
   end
 
